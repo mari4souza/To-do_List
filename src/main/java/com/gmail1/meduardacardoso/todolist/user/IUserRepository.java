ecc3177bf7f4f64120213f2dce239430b6695643
mari4souza <meduardacardoso.121@gmail.com>
2023-10-17 14:54:09 -0300

    Tudo que fiz até a aula 4

---
+++ src/main/java/com/gmail1/meduardacardoso/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.gmail1.meduardacardoso.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
